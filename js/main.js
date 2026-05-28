4ad8504fba8807a2bf1d3383253af2a6f8d463d6
Fe1aa <emir.felic2009@gmail.com>
2026-05-28 21:11:10 +0200

    Refactor styles and update game logic for improved functionality and aesthetics

--- js/main.js
+++ js/main.js
@@ -2,34 +2,45 @@ var circle = document.getElementById("circle");
 var showTime = document.getElementById("time");
 var scoreCount = document.getElementById("score");
 var timeStart = new Date().getTime();
+var timeBetween = 2000;
+var circleSize = 250;
+var editCount = 20;
 var score = 0;
+const colors = ["#FFFFF0", "#D4AF37", "#B87333", "#FFB7C5", "#8F9E8B"];
 
 function showCircle() {
   circle.style.display = "block";
   circle.style.top = Math.random() * 500 + "px";
   circle.style.left = Math.random() * 900 + "px";
   circle.style.backgroundColor = randomColor();
+  circle.style.width = circleSize + "px";
+  circle.style.height = circleSize + "px";
   timeStart = new Date().getTime();
+}
+
+function updateVariables() {
+  if(editCount <= 0) {
+    return;
+  }
+  editCount--;
+  timeBetween -= 40;
+  circleSize -= 10;
 }
 
 showCircle();
 
 circle.onclick = function() {
   circle.style.display = "none";
-  setTimeout(showCircle, 1000);
+  setTimeout(showCircle, timeBetween);
   var timeEnd = new Date().getTime();
   var time = (timeEnd - timeStart) / 1000;
   score++;
+  updateVariables();
   showTime.innerHTML = time + "s";
   scoreCount.innerHTML = "Score: " + score;
 }
 
 function randomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
+  var color = colors[Math.floor(Math.random() * colors.length)];
   return color;
 }
